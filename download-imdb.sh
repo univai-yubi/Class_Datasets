9a5d5700b98ef9550fe3468bde08377efa23cdf9
Rahul Dave <rahuldave@gmail.com>
2023-01-30 20:00:47 -0500

    added some files

---
+++ download-imdb.sh
@@ -0,0 +1,5 @@
+curdir=`pwd`
+cd data
+wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+tar xvfz aclImdb_v1.tar.gz
+cd $curdir
---
+++ download_glove.sh
@@ -0,0 +1,8 @@
+curdir=`pwd`
+cd data
+mkdir glove
+cd glove
+# need to download glove from http://nlp.stanford.edu/data/glove.6B.zip
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
+cd $curdir
