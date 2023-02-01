304d731b97f0cb702d6d4c609b0e8b05223822c6
Rahul Dave <rahuldave@gmail.com>
2023-02-01 14:22:57 -0500

    added data

--- download-imdb.sh
+++
@@ -1,6 +0,0 @@
-curdir=`pwd`
-mkdir -p tmp
-cd tmp
-wget http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
-tar xvfz aclImdb_v1.tar.gz
-cd $curdir
--- download_glove.sh
+++
@@ -1,9 +0,0 @@
-curdir=`pwd`
-mkdir -p tmp
-cd tmp
-mkdir glove
-cd glove
-# need to download glove from http://nlp.stanford.edu/data/glove.6B.zip
-wget http://nlp.stanford.edu/data/glove.6B.zip
-unzip glove.6B.zip
-cd $curdir
